f42383d1506b1494d7dcb473a891fa47e255cf7e
Abraham José González Barboza <jgab.net@gmail.com>
2017-12-28 09:54:58 -0300

    feat: show contingency on all routes

--- src/js/lib/toolbar.js
+++ src/js/lib/toolbar.js
@@ -59,7 +59,7 @@
 
       //play simulation
       $listen.on('click', function (e) {
-        e.preventDefault()
+        e.preventDefault();
         $(this).addClass('invisible');
         $toolbarPlayer.removeClass('invisible')
       });
