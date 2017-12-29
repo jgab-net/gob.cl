53e0687113c97f6eb3b1be51d8ec813c794c443f
Abraham José González Barboza <jgab.net@gmail.com>
2017-12-29 16:36:39 -0300

    feat: a11y-tts in progress

--- src/js/lib/toolbar.js
+++ src/js/lib/toolbar.js
@@ -161,5 +161,34 @@
         $.data(this, pluginDataKey).setOptions(options);
       }
     });
-  }
+  };
+
+  if (! window.rsConf) { window.rsConf = {}; }
+  if (! window.rsConf.ui) { window.rsConf.ui = {}; }
+  window.rsConf.ui.rsbtnClass = 'rsbtn-gobcl-skin';
+  window.rsConf.ui.player = [
+    '<span class="rsbtn_box">',
+    '	<a href="javascript:void(0);" class="rsbtn_pause rsimg rspart rsbutton" title="Pause">',
+    '		<span class="rsbtn_btnlabel">Pause</span>',
+    '	</a>',
+    '	<span class="rsbtn_progress_container rspart">',
+    '		<span class="rsbtn_progress_played"></span>',
+    '	</span>',
+    '	<a href="javascript:void(0);" class="rsbtn_dl rsimg rspart rsbutton" title="Download mp3">',
+    '		<span class="rsbtn_btnlabel">Download mp3</span>',
+    '	</a>',
+    '	<a href="javascript:void(0);" class="rsbtn_closer rsimg rspart rsbutton" title="Close player">',
+    '		<span class="rsbtn_btnlabel">Close</span>',
+    '	</a>',
+    '	<span class="rsdefloat"></span>',
+    '</span>'
+  ];
+
+  window.rsConf.ui.popupbutton = [
+    '<a class="rsbtn_play" accesskey="L" title="Listen with ReadSpeaker" href="">',
+    '<span class="rsbtn_left rspart"><span class="rsbtn_text"><span>Listen</span></span></span>',
+    '<span class="rsbtn_right rsimg rsplay rspart"></span>',
+    '</a>'
+  ];
+
 })(jQuery, window, document);
