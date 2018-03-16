c3d87d7eebe9e8f73860f3565bd739503f8ceb1a
Abraham José González Barboza <jgab.net@gmail.com>
2018-03-16 13:39:34 -0700

    feat: infinite scroll and readspeaker compatibility

--- src/js/app.js
+++ src/js/app.js
@@ -1,74 +1,85 @@
-"use strict";
-var App = {};
+'use strict';
 
 $(function () {
-  App.init();
+  GobCl.init();
 });
 
-App = {
-  init: function () {
-    this.searchField();
-    this.search();
-    this.tags();
-    this.a11y();
-    this.contingency();
-    this.imgToBackground();
-  },
-  a11y: function () {
-    $('.toolbar').toolbar();
-  },
-  isDesktop: function () {
-    return $(window).width() >= 768;
-  },
-  searchField: function () {
-    $('.search-form').searchField();
-  },
-  tags: function () {
-    $('.tags').tags();
-  },
-  imgToBackground: function () {
-    $('.elem-bg').each(function (index, el) {
-      var srcImg = $(el).find('.img-to-bg').attr('src');
-      if (srcImg) {
-        $(el).css('background-image', 'url(' + srcImg + ')');
-      }
-    });
-  },
-  search: function () {
-    var $inputSearch = $('#input-search');
-    var $search = $('#search');
-    var $searchHero = $('#searchHero');
-    var searchContent = $search.find('.search-form_content');
-    var searchHeroContent = $searchHero.find('.search-form_content');
+var GobCl = (function () {
 
-    $('.toggle-search').on('click', function (e) {
-      var $body = $('body').toggleClass('search-active');
-      if ($body.hasClass('search-active')) {
-        $inputSearch.focus();
-        if ($searchHero.length > 0) {
-          var posSearch = $(searchHeroContent).offset().top - $(searchContent).offset().top;
-          $(searchHeroContent).css('margin-top', -(posSearch));
-          setTimeout(function () {
-            $(searchHeroContent).css('margin-top', 0);
-          }, 700);
+  var GobClBuilder = {
+    a11y: function () {
+      $('.toolbar').toolbar();
+    },
+    isDesktop: function () {
+      return $(window).width() >= 768;
+    },
+    searchField: function () {
+      $('.search-form').searchField();
+    },
+    tags: function () {
+      $('.tags').tags();
+    },
+    imgToBackground: function () {
+      $('.elem-bg').each(function (index, el) {
+        var srcImg = $(el).find('.img-to-bg').attr('src');
+        if (srcImg) {
+          $(el).css('background-image', 'url(' + srcImg + ')');
         }
-      } else {
-        if ($searchHero.length > 0) {
-          var posSearchHero = $(searchHeroContent).offset().top - $(searchContent).offset().top;
-          $(searchContent).css('margin-top', posSearchHero);
-          setTimeout(function () {
-            $(searchContent).css('margin-top', 0);
-          }, 700);
+      });
+    },
+    search: function () {
+      var $inputSearch = $('#input-search');
+      var $search = $('#search');
+      var $searchHero = $('#searchHero');
+      var searchContent = $search.find('.search-form_content');
+      var searchHeroContent = $searchHero.find('.search-form_content');
+
+      $('.toggle-search').on('click', function (e) {
+        var $body = $('body').toggleClass('search-active');
+        if ($body.hasClass('search-active')) {
+          $inputSearch.focus();
+          if ($searchHero.length > 0) {
+            var posSearch = $(searchHeroContent).offset().top - $(searchContent).offset().top;
+            $(searchHeroContent).css('margin-top', -(posSearch));
+            setTimeout(function () {
+              $(searchHeroContent).css('margin-top', 0);
+            }, 700);
+          }
+        } else {
+          if ($searchHero.length > 0) {
+            var posSearchHero = $(searchHeroContent).offset().top - $(searchContent).offset().top;
+            $(searchContent).css('margin-top', posSearchHero);
+            setTimeout(function () {
+              $(searchContent).css('margin-top', 0);
+            }, 700);
+          }
         }
-      }
-    });
-  },
-  scroller: function () {
-    $(".nano").nanoScroller({
-      alwaysVisible: true
-    });
-  },
-  contingency: function () {
-    $('.contingency').contingency();
+      });
+    },
+    scroller: function () {
+      $(".nano").nanoScroller({
+        alwaysVisible: true
+      });
+    },
+    contingency: function () {
+      $('.contingency').contingency();
+    },
+    build: function () {
+      this.searchField();
+      this.search();
+      this.tags();
+      this.a11y();
+      this.contingency();
+      this.imgToBackground();
+    }
+  };
+
+  return {
+    init: function () {
+      GobClBuilder.build();
+    },
+    closeReadSpeaker: function () {
+      $('.rsbtn_closer').click();
+    }
   }
-};
+})();
---
+++ src/js/lib/load-more.js
@@ -0,0 +1,98 @@
+/* global $, document, moment, templates */
+
+/**
+ * createInifiniteScroll - This function generates a creator of an infinite scroll
+ * @param {string} urlTemplate - a link for ajax request
+ * @param {function} urlTransformation -  function that adds additional parameters
+ * to a requestUrl (optional), should accept url as parameter and return a new url.
+ * Will be used only at first call, after it we just use what server sends us a 'next'
+ * @param {string} templateName - a pug template which will be used to render results from server
+ * @param {string} targetSelector - a selector to an existing container
+ * @param {function} postLoadingTransformation - a function that is caused in the end
+ * of the procedure (optional)
+ * @returns {function} - a function that creates an infinite scroll
+ */
+GobCl.createInifiniteScroll = function (
+  urlTemplate,
+  urlTransformation,
+  templateName,
+  targetSelector,
+  postLoadingTransformation
+) {
+  return function () {
+    // adding additional parameter to a request using a function-parameter
+    var requestUrl = urlTransformation ? urlTransformation(urlTemplate) : urlTemplate;
+
+    var isAlreadySent = false;
+    var blockFutureRequests = false;
+    var $loadingIndicator = $('.loading-indicator');
+    $loadingIndicator.hide();
+
+    document.addEventListener('scroll', function () {
+      // This checks prevent additional request while the already sent one is not resolved
+      if (isAlreadySent) {
+        return;
+      }
+
+      // Here we check if a loading indicator is inside our viewport. If it is true
+      // We can call a request
+      var docViewTop = $(window).scrollTop();
+      var docViewBottom = docViewTop + $(window).height();
+
+      // we use parent of an indicator because hidden elements do not have a height
+      // but their container still has
+      var elemTop = $loadingIndicator.parent().offset().top;
+      var elemBottom = elemTop + $loadingIndicator.height();
+
+      var shouldLoadMore = ((elemBottom <= docViewBottom - 10 )
+        && (elemTop >= docViewTop));
+
+      if (shouldLoadMore && !blockFutureRequests) {
+        isAlreadySent = true;
+        $loadingIndicator.show();
+
+        GobCl.closeReadSpeaker();
+
+        $.ajax(requestUrl, {
+          success: function (response) {
+            // Setting a link for a consequent request
+            requestUrl = response.next || requestUrl;
+            var currentLanguage = response.current_language;
+
+            // transforming a publishing date to a readable format for all results
+            var articles = response.results.map(function (article) {
+              var format;
+              if (currentLanguage === 'es') {
+                format = 'LL';
+              } else if (currentLanguage === 'en') {
+                format = 'll';
+              }
+              moment.locale(currentLanguage);
+              article.publishing_date =  moment(article.publishing_date).format(format);
+              return article;
+            });
+
+            // Generating DOM using a pug-template
+            var newContent = templates[templateName]({
+              articles: articles,
+              currentLanguage: currentLanguage
+            });
+
+            // Appending it to a current container
+            $(targetSelector).append(newContent);
+            isAlreadySent = false;
+            $loadingIndicator.hide();
+
+            // post loading transformatiom
+            postLoadingTransformation && postLoadingTransformation();
+
+            // If no more articles is loaded we block this function forever
+            if (response.results.length === 0) {
+              blockFutureRequests = true;
+            }
+          }
+        });
+      }
+    });
+  };
+};
